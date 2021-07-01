9346a67e68c885a77277652d62384c6d2b9d4e17
danielpellegrini <danielpellegrini87@gmail.com>
2021-07-01 10:32:50 +0200

    fixed contact page

--- include/google-map-1.php
+++ include/google-map-1.php
@@ -1,6 +1,4 @@
 		<div class="template-content-section template-padding-top-reset template-padding-bottom-reset">
-			
 			<!-- Google map -->
-			<div class="template-component-google-map" id="template-component-google-map-1"></div>
-		
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d624.2252472102634!2d7.148136929265584!3d51.25773233801249!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8d7b3a13cb9d5%3A0x3cfc1c7230cf30dc!2sMorianstra%C3%9Fe%2018-20%2C%2042103%20Wuppertal!5e0!3m2!1sen!2sde!4v1625127275282!5m2!1sen!2sde" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 		</div>
